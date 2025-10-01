77008e2684b4f4f9cb621d8e1d4e0aedb3243c25
leasabus <leandro.sabus@gmail.com>
2025-10-01 15:49:54 -0300

    add create a new product in supplier forms

--- components/forms/AddSupplierForm.tsx
+++ components/forms/AddSupplierForm.tsx
@@ -15,6 +15,8 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { toast } from "sonner";
 import { Trash } from "lucide-react";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { AddIngredientForm } from "./AddIngredientForm";
 
 interface AddSupplierFormProps {
   products: Product[];
@@ -25,6 +27,8 @@ export const AddSupplierForm: React.FC<AddSupplierFormProps> = ({
   products,
   onClose,
 }) => {
+  const [isAddProductDialogOpen, setIsAddProductDialogOpen] = useState(false);
+
   const [formData, setFormData] = useState<CreateSupplierDto>({
     name: "",
     email: "",
@@ -80,104 +84,125 @@ export const AddSupplierForm: React.FC<AddSupplierFormProps> = ({
   };
 
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
-      <div>
-        <Label htmlFor="name">Nombre del proveedor</Label>
-        <Input
-          id="name"
-          value={formData.name}
-          onChange={(e) =>
-            setFormData((prev) => ({ ...prev, name: e.target.value }))
-          }
-          required
-        />
-      </div>
+    <>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <Label htmlFor="name">Nombre del proveedor</Label>
+          <Input
+            id="name"
+            value={formData.name}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, name: e.target.value }))
+            }
+            required
+          />
+        </div>
 
-      <div>
-        <Label htmlFor="email">Email</Label>
-        <Input
-          id="email"
-          type="email"
-          value={formData.email}
-          onChange={(e) =>
-            setFormData((prev) => ({ ...prev, email: e.target.value }))
-          }
-          required
-        />
-      </div>
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, email: e.target.value }))
+            }
+            required
+          />
+        </div>
 
-      <div>
-        <Label htmlFor="phone">Teléfono</Label>
-        <Input
-          id="phone"
-          value={formData.phone}
-          onChange={(e) =>
-            setFormData((prev) => ({ ...prev, phone: e.target.value }))
-          }
-          required
-        />
-      </div>
+        <div>
+          <Label htmlFor="phone">Teléfono</Label>
+          <Input
+            id="phone"
+            value={formData.phone}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, phone: e.target.value }))
+            }
+            required
+          />
+        </div>
 
-      <div>
-        <Label htmlFor="address">Dirección</Label>
-        <Input
-          id="address"
-          value={formData.address}
-          onChange={(e) =>
-            setFormData((prev) => ({ ...prev, address: e.target.value }))
-          }
-          required
-        />
-      </div>
+        <div>
+          <Label htmlFor="address">Dirección</Label>
+          <Input
+            id="address"
+            value={formData.address}
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, address: e.target.value }))
+            }
+            required
+          />
+        </div>
 
-      <div>
-        <Label>Productos</Label>
-        <Select
-          value=""
-          onValueChange={(value) => handleAddProduct(Number(value))}
-        >
-          <SelectTrigger className="w-[200px]">
-            <SelectValue placeholder="Agregar Producto" />
-          </SelectTrigger>
-          <SelectContent>
-            {products.map((product) =>
-              product.id ? (
-                <SelectItem key={product.id} value={product?.id.toString()}>
-                  {product.name}
-                </SelectItem>
-              ) : null
-            )}
-          </SelectContent>
-        </Select>
+        <div>
+          <Label>Productos</Label>
+          <Select
+            value=""
+            onValueChange={(value) => handleAddProduct(Number(value))}
+          >
+            <SelectTrigger className="w-[200px]">
+              <SelectValue placeholder="Agregar Producto" />
+            </SelectTrigger>
+            <SelectContent>
+              {products.map((product) =>
+                product.id ? (
+                  <SelectItem key={product.id} value={product?.id.toString()}>
+                    {product.name}
+                  </SelectItem>
+                ) : null
+              )}
+            </SelectContent>
+          </Select>
 
-        {formData.products.map((sp) => (
-          <div key={sp.productId} className="flex items-center gap-2 mt-2">
-            <span>{products.find((p) => p.id === sp.productId)?.name}</span>
-            <Label className="ml-2 text-red-500">Precio Costo:</Label>
-            <Input
-              type="number"
-              min={1}
-              value={sp.costPrice}
-              onChange={(e) =>
-                handleCostPriceChange(sp.productId, Number(e.target.value))
-              }
-              placeholder="Precio costo"
-              className="w-24"
-            />
-            <Button
-              type="button"
-              variant="destructive"
-              onClick={() => handleRemoveProduct(sp.productId)}
-            >
-              <Trash />
-            </Button>
-          </div>
-        ))}
-      </div>
+          <p
+            className="text-sm mt-1 text-blue-500 cursor-pointer hover:underline"
+            onClick={() => setIsAddProductDialogOpen(true)}
+          >
+            ¿No existe el producto? Crear.
+          </p>
 
-      <Button type="submit" className="w-full mt-4">
-        Crear Proveedor
-      </Button>
-    </form>
+          {formData.products.map((sp) => (
+            <div key={sp.productId} className="flex items-center gap-2 mt-2">
+              <span>{products.find((p) => p.id === sp.productId)?.name}</span>
+              <Label className="ml-2 text-red-500">Precio Costo:</Label>
+              <Input
+                type="number"
+                min={1}
+                value={sp.costPrice}
+                onChange={(e) =>
+                  handleCostPriceChange(sp.productId, Number(e.target.value))
+                }
+                placeholder="Precio costo"
+                className="w-24"
+              />
+              <Button
+                type="button"
+                variant="destructive"
+                onClick={() => handleRemoveProduct(sp.productId)}
+              >
+                <Trash />
+              </Button>
+            </div>
+          ))}
+        </div>
+
+        <Button type="submit" className="w-full mt-4">
+          Crear Proveedor
+        </Button>
+      </form>
+
+      <Dialog
+        open={isAddProductDialogOpen}
+        onOpenChange={setIsAddProductDialogOpen}
+      >
+        <DialogContent className="max-w-2xl max-h-[90vh] overflow-y-auto mx-4">
+          <DialogHeader>
+            <DialogTitle>Agregar Nuevo Producto</DialogTitle>
+          </DialogHeader>
+          <AddIngredientForm onClose={() => setIsAddProductDialogOpen(false)} />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
